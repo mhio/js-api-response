7812096042dc352aac30cd215342628e5f3d71a5
Matt Hoyle <code@deployable.co>
2018-01-18 13:14:35 +0000

    Response
    Change Response API, static `.json`, `.raw`, `.template` methods
    
    Use ClassDebug

--- Response.js
+++ Response.js
@@ -1,20 +1,57 @@
-const debug = require('debug')('mh:Api:Response')
+const ClassDebug = require('@mhp/ClassDebug').default
 const { Message, MessageData } = require('@mhp/api-message')
-
+const defaults = require('lodash/defaults')
 
 // ## Response
 
-// Hide express/socketio from the handlers
-
 class Response {
 
+  static classInit(){
+    ClassDebug.setup(this, 'mh:api')
+    this.prototype.message = this.prototype.setMessage
+  }
+
+  static json( data, options ){
+    let opts = { type: 'json', message: data }
+    defaults(opts, options)
+    return new Response(opts)
+  }
+
+  static raw( message, options ){
+    let opts = { type: 'raw', message: message }
+    defaults(opts, options)
+    return new Response(opts)
+  }
+
+  static template( locals, options ){
+    let opts = { type: 'template', message: locals }
+    defaults(opts, options)
+    return new Response(opts)
+  }
+
   constructor( options ){
-    if (!options) throw new Error('No options passed in')
-    this._status = options.status || 200
-    this._message = options.message // string for raw or object for json/template
+    if ( !options ) throw new Error('No options passed to Response')
+    this._types = [ 'raw', 'template', 'json' ]
+
+    this.setType( options.type || 'raw' ) // 'template', 'json'
     this._template = options.template
+    this._status = options.status || 200
     this._headers = {}
-    this._type = 'raw' // 'template', 'json'
+    if ( options.hasOwnProperty('message') ){ 
+      this.setMessage(options.message) // string for raw or object for json/template
+    }
+  }
+
+  json( message ){
+    this._type = 'json'
+    if ( message === undefined ) {
+      this.debug('No JSON message, just setting type')
+      return this
+    }
+    this._message = ( message instanceof Message )
+      ? message
+      : new MessageData(message)
+    return this
   }
 
   // headers go in the message for socketio?
@@ -23,43 +60,50 @@ class Response {
     return this
   }
 
-  message( message ){
-    this._message = message
-    return this
-  }
-
-  json( message ){
-    this._type = 'json'
-    if (message !== undefined) {
-      this._message = ( message instanceof Message )
-        ? message
-        : new MessageData(message)
-    }
-    return this
-  }
-
   setTemplate( name ){
     this._template = name
     return this
   }
 
-  template( message ){
-    this._type = 'template'
-    if (message !== undefined) this._message = message
-    return this
+  setMessage( message ){
+    switch ( this._type ){
+      case 'raw':
+        if ( ! message instanceof String && ! message instanceof Buffer ) {
+          throw new Error('Raw responses must be a String or Buffer')
+        }
+        this._message = message
+        return this
+
+      case 'json':
+        this._message = ( message instanceof Message )
+          ? message
+          : new MessageData(message)
+        return this
+
+      case 'template':
+        this._message = ( message instanceof Message )
+          ? message.data
+          : message
+        return this
+    
+      default:
+        this._message = message
+        return this
+    }
   }
 
-  raw( message ){
-    this._type = 'raw'
-    if (message !== undefined) this._message = message
-    return this
-  }
-
-  debug(){
-    debug(this)
-    return this
+  setType( type ){
+    if ( ! this._types.includes( type ) ) {
+      throw new Error(`Type "${type}" not in [ ${this._types.join(', ')}]`)
+    }
+    // Convert an existing message to json
+    if ( type === 'json' && ! this._message instanceof Message ) {
+      this._message = new MessageData(this._message)
+    }
+    this._type = type
   }
 
 }
+Response.classInit()
 
-module.exports = { Response }
+module.exports = { Response, Message, MessageData }
--- test/unit/Response.js
+++ test/unit/Response.js
@@ -1,14 +1,110 @@
-const { Response } = require('../../Response.js')
+const { Response, Message } = require('../../Response.js')
 
-describe('mh::test::Unit::Response', function () {
+describe('mh::test::Unit::Response', function(){
   
-  it('should have a Response', function() {
+  it('should have a Response', function(){
     expect(Response).to.be.ok
   })
 
-  it('should create a Response', function() {
+  it('should create a Response', function(){
     let r = new Response({ message: {} })
     expect(r).to.be.ok
+  })
+
+  it('should fail to create a Response without data', function(){
+    let fn = ()=> new Response()
+    expect( fn ).to.throw(/No options passed to Response/)
+  })
+
+
+  describe('Response instance', function(){
+    
+    let resp = null
+
+    beforeEach(function(){
+      resp = new Response({ message: { data: true }})
+    })
+
+    it('should set a message', function(){
+      resp.setMessage('<html><body></body></html>')
+      expect( resp._message ).to.equal('<html><body></body></html>')
+    })
+
+    it('should set a json message', function(){
+      let ret = resp.json({ some: 'data' })
+      //expect( resp._message ).to.eql({})
+      expect( resp._message ).to.have.property('id').and.be.a('string')
+      expect( resp._message ).to.have.property('ts').and.be.a('number')
+      expect( resp._message ).to.have.property('data')
+      expect( resp._message.data ).to.eql({ some: 'data' })
+      expect( ret ).to.eql( resp )
+    })
+
+    it('should set a Message as json message', function(){
+      let msg = new Message({ some: 'data' })
+      let ret = resp.json(msg)
+      //expect( resp._message ).to.eql({})
+      expect( resp._message ).to.eql(msg)
+      expect( ret ).to.eql( resp )
+    })
+
+    it('should set a header', function(){
+      let ret = resp.setHeader('meee', 'youuuu')
+      expect( resp._headers['meee'] ).to.equal( 'youuuu' )
+      expect( ret ).to.eql( resp )
+    })
+
+    it('should set a template', function(){
+      let ret = resp.setTemplate('tmplname')
+      expect( resp._template ).to.equal( 'tmplname' )
+      expect( ret ).to.eql( resp )
+    })
+
+    })
+
+  describe('Response json instance', function(){
+
+    let resp = null
+
+    beforeEach(function(){
+      resp = Response.json({ myval: true })
+    })
+
+    it('should set the Response type to json with no message', function(){
+      expect( resp._type ).to.equal( 'json' )
+      expect( resp._message ).to.have.property('data').and.eql({ myval: true })
+    })
+
+  })
+
+  describe('Response raw instance', function(){
+
+    let resp = null
+
+    beforeEach(function(){
+      resp = Response.raw('stringy string')
+    })
+
+    it('should set type to raw', function(){
+      expect( resp._type ).to.equal( 'raw' )
+      expect( resp._message ).to.equal( 'stringy string' )
+    })
+
+  })
+
+  describe('Response template instance', function(){
+
+    let resp = null
+
+    beforeEach(function(){
+      resp = Response.template({ somedata: true })
+    })
+
+    it('should set type to template', function(){
+      expect( resp._type ).to.equal( 'template' )
+      expect( resp._message ).to.eql({ somedata: true })
+    })
+
   })
 
 })
