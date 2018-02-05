7e0473bd03eab62e1a1f87e9e6e0268a2f10e01c
Matt Hoyle <code@deployable.co>
2018-02-05 23:48:12 +0000

    test module exports

--- test/unit/module.js
+++ test/unit/module.js
@@ -1,9 +1,21 @@
-const ApiMessage = require('../../')
+const { ApiResponse, Message, MessageData, MessageError }  = require('../../')
 
-describe('ApiMessage', function(){
+describe('module/package', function(){
 
-  it('should load module', function(){
-    expect( ApiMessage ).to.be.ok
+  it('should load ApiResponse from module', function(){
+    expect( ApiResponse ).to.be.ok
+  })
+
+  it('should load Message from module', function(){
+    expect( Message ).to.be.ok
+  })
+
+  it('should load MessageData from module', function(){
+    expect( MessageData ).to.be.ok
+  })
+
+  it('should load MessageError from module', function(){
+    expect( MessageError ).to.be.ok
   })
 
 })
